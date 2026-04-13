738a56b576481c0fb376297fc1ed7a748ee07b6d
Hiroya Fujinami <make.just.on@gmail.com>
2026-04-14 06:03:06 +0900

    fix: remove preprocessors

--- ruby-prototype/lib/naraku_ruby.rb
+++ ruby-prototype/lib/naraku_ruby.rb
@@ -1,18 +1,8 @@
 require_relative 'naraku_ruby/mruby_bridge'
 require_relative 'naraku_ruby/errors'
-require_relative 'naraku_ruby/preprocessor'
-require_relative 'naraku_ruby/preprocessors/group_number_resolver'
-require_relative 'naraku_ruby/preprocessors/group_ref_resolver'
-require_relative 'naraku_ruby/preprocessors/empty_match_analyzer'
 
 module NarakuRuby
   ENUM_VALUE_KEYS = %i[type assertion_type quantifier_type char_type posix_char_class].freeze
-
-  PREPROCESSORS = [
-    Preprocessors::GroupNumberResolver.new,
-    Preprocessors::GroupRefResolver.new,
-    Preprocessors::EmptyMatchAnalyzer.new,
-  ].freeze
 
   def self.parse(pattern, **options)
     bridge = MRubyBridge.new(script_path: File.expand_path('./mruby-scripts/parse.rb', __dir__))
@@ -23,13 +13,6 @@ module NarakuRuby
     context = bridge.execute(request)
     normalize_enum_values!(context)
     context
-  end
-
-  def self.preprocess(context)
-    return context unless context.is_a?(Hash)
-    return context unless context[:node].is_a?(Hash)
-
-    PREPROCESSORS.reduce(context) { |ctx, preprocessor| preprocessor.call(ctx) }
   end
 
   def self.normalize_enum_values!(obj)
---
+++ ruby-prototype/tests/test_parse.rb
@@ -0,0 +1,54 @@
+require_relative 'test_helper'
+
+class TestParse < Minitest::Test
+  def test_normalizes_node_type_values_to_symbols
+    result = NarakuRuby.parse('(?=a)\w*[[:digit:]]')
+    node = result[:node]
+
+    refute_nil node
+    assert_instance_of Symbol, node[:type]
+    assert_equal :concat, node[:type]
+
+    assertion = first_node(node) { |n| n[:type] == :assertion }
+    quantifier = first_node(node) { |n| n[:type] == :quantifier }
+    char_type = first_node(node) { |n| n[:type] == :char_type }
+    posix_item = first_node(node) { |n| n[:type] == :posix_char_class }
+
+    assert_equal :positive_lookahead, assertion[:assertion_type]
+    assert_equal :greedy, quantifier[:quantifier_type]
+    assert_equal :word, char_type[:char_type]
+    assert_equal :digit, posix_item[:posix_char_class]
+  end
+
+  def test_keeps_non_enum_values_as_is
+    result = NarakuRuby.parse('(?<foo>a)\k<foo>')
+    group = first_node(result[:node]) { |n| n[:type] == :group }
+    back_ref = first_node(result[:node]) { |n| n[:type] == :back_ref }
+
+    assert_instance_of String, group[:name]
+    assert_instance_of String, back_ref[:name]
+    assert_equal 'foo', group[:name]
+    assert_equal 'foo', back_ref[:name]
+  end
+
+  private
+
+  def first_node(node, &)
+    find_nodes(node, &).first
+  end
+
+  def find_nodes(node, &block)
+    nodes = []
+    if node.is_a?(Hash)
+      nodes << node if block.call(node)
+      node.each_value do |value|
+        if value.is_a?(Hash)
+          nodes.concat(find_nodes(value, &block))
+        elsif value.is_a?(Array)
+          value.each { |child| nodes.concat(find_nodes(child, &block)) }
+        end
+      end
+    end
+    nodes
+  end
+end
